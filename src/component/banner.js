d0c4eb32eda04807f795b2880ab8464f41ffd5ea
Asvan-Launcher <95467525+Amzi-kwasi@users.noreply.github.com>
2025-09-19 21:31:09 +0000

    df

--- src/component/banner.js
+++ src/component/banner.js
@@ -35,7 +35,7 @@ export default function Banner(){
     </div>
   </section> */}
 
-<video src={require("/workspaces/-/src/video/mec.mp4")} style={{width:"100%"}} muted autoPlay loop></video>
+<video src={require("/video/mec.mp4")} style={{width:"100%"}} muted autoPlay loop></video>
      
         </>
     )
