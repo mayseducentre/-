b4ca4b4c5bb846a05584491c8548807b8f8fa18b
Asvan-Launcher <95467525+Amzi-kwasi@users.noreply.github.com>
2025-09-21 14:23:58 +0000

    gha

--- src/component/footer.js
+++ src/component/footer.js
@@ -40,6 +40,7 @@ export default function Footer(){
               <li><i className="fa fa-chevron-right"></i><a href="#/assess">Assessment Sheet</a></li>
               <li><i className="fa fa-chevron-right"></i><a onClick={getaccess}>AAS</a></li>
               <li><i className="fa fa-chevron-right"></i><a href="#/digital_note">Digital Note</a></li>
+              <li><i className="fa fa-chevron-right"></i><a href="/autoe.html" target="_blank" rel="noopener noreferrer">Exam Gen</a></li>
             </ul>
           </div>
 
