2c3c699324743ffae8c3c00d044df6f33802d36a
Asvan-Launcher <95467525+Amzi-kwasi@users.noreply.github.com>
2025-10-05 17:07:49 +0000

    T

--- src/component/breadcrumb.js
+++ src/component/breadcrumb.js
@@ -1,7 +1,7 @@
 export default function Breadcrumb(props){
 
     return(
-        <section className="breadcrumb-option" style={{background:`linear-gradient(rgba(0,0,0,0.5), rgba(0,0,0,0.5)), url(${props.image})`}}>
+        <section className="breadcrumb-option" style={{backgroundImage:`linear-gradient(rgba(0,0,0,0.5), rgba(0,0,0,0.5)), url(${props.image})`}}>
         <div className="container">
             <div className="row">
                 <div className="col-lg-12">
