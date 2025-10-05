cb2f098a79c68d61beb4b1c5e7a35258efdae444
Asvan-Launcher <95467525+Amzi-kwasi@users.noreply.github.com>
2025-10-05 18:58:37 +0000

    Ac

--- src/pages/account.js
+++ src/pages/account.js
@@ -10,7 +10,7 @@ export default function Account(){
         <>
         <Header />
         <br/>
-        <Breadcrumb title="Account" />
+        <Breadcrumb title="Account" image="https://lh3.googleusercontent.com/pw/AP1GczPQGOKEU8vsI_m9AbosnfoxCrLM_a2kgCphO6x_1SOhxHHw0o87A8ZAUg1aUoGr-8NQrTZK0T41ZaZsibhQhDVNMJDdy9SggXHiTtmeWT9pnKrq1P0"/>
         {/* <Notify /> */}
         <Accountform />
         <ScrollToTop smooth className="scrolly" />
