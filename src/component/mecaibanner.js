1bb51a73bd10ab0159e2cdc297124dfca8ddc6b3
Asvan-Launcher <95467525+Amzi-kwasi@users.noreply.github.com>
2025-10-23 18:44:36 +0000

    Change image source to an empty string
    Updated image source in mecaibanner component.

--- src/component/mecaibanner.js
+++ src/component/mecaibanner.js
@@ -112,7 +112,7 @@ const MecAIBanner = () => {
           <div style={styles.modal}>
             <div style={{ position: "relative" }}>
               <img
-                src="https://lh3.googleusercontent.com/pw/AP1GczNermM-f3HxJvCB44F3AmU131KhZi9RnwZ5jY7fG_vMz1BwGlW4fv5WgXghGkbtjwzKToC9gxuUeSx5SiojyaK0osv_jxJ0A74GsfoK21G1F_P2Aww"
+                src=""
                 alt="MEC AI Banner"
                 style={styles.banner}
               />
