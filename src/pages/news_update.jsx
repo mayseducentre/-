e2eca246a21af35153815328ac76825d8835c67a
Asvan-Launcher <95467525+Amzi-kwasi@users.noreply.github.com>
2025-11-01 18:22:55 +0000

    Refactor News component and update data fetching

--- src/pages/news_update.jsx
+++ src/pages/news_update.jsx
@@ -3,103 +3,117 @@ import Breadcrumb from "../component/breadcrumb";
 import Footer from "../component/footer";
 import Header from "../component/header";
 import { useEffect, useState } from "react";
-import Skeleton from "react-loading-skeleton"
-import "react-loading-skeleton/dist/skeleton.css"
+import Skeleton from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
 import NewsInfo from "../component/news_info";
 
+export default function News() {
+  function handleInfo(blogger) {
+    document.getElementById("newsinfo").style.display = "block";
+    document.getElementById("recent-posts").style.display = "none";
 
-var path=process.env.REACT_APP_BLOG_API;
-export default function News(){
-
-  function handleInfo(blogger){
-    document.getElementById("newsinfo").style.display="block"
-    document.getElementById("recent-posts").style.display="none";
-
-    
-    document.getElementById("newscategory").innerHTML=blogger.news_category;
-    document.getElementById("newscontent").value=blogger.news_content;
-    document.getElementById("newsheader").innerHTML=blogger.news_heading;
-    document.getElementById("newsdate").innerHTML=blogger.date;
-    document.getElementById("newsimg").src=blogger.news_img;
-    
-    
+    document.getElementById("newscategory").innerHTML = blogger.news_category;
+    document.getElementById("newscontent").value = blogger.news_content;
+    document.getElementById("newsheader").innerHTML = blogger.news_heading;
+    document.getElementById("newsdate").innerHTML = blogger.date;
+    document.getElementById("newsimg").src = blogger.news_img;
   }
 
-  const [blog, setBlog]=useState([]);
-  const [loading, setLoading]=useState(false)
+  const [blog, setBlog] = useState([]);
+  const [loading, setLoading] = useState(false);
 
-  useEffect(()=>{
-fetch(`${path}/blog`)
-.then(res => res.json())
-.then(data => {setBlog(data)
-  setLoading(true)
-})
-.catch(err => console.log(err))
-  },[])
-    return(
-        <>
-        <Header />
-        <Breadcrumb title="News and Updates" image="https://lh3.googleusercontent.com/pw/AP1GczPUCocKMxC4wGVcuARVjp01biMREjUpBLF8kb06utd3X07oBJV-Avdh3tiqrcCtff9ZoFv2FDL3Cntewt5Kq6NDxapUD8XKAf8RIR-9s2EwdWKfLaw"/>
-      <br/>
-<br/>
+  useEffect(() => {
+    // Try to load news data from local db.json file
+    fetch("/db.json")
+      .then((res) => res.json())
+      .then((data) => {
+        // If your db.json has a section for news, adjust accordingly
+        // Example assumes you stored blog posts under "blog" or "library[0].news"
+        const blogs = data.blog || data.library?.[0]?.blog || [];
+        setBlog(blogs);
+        setLoading(true);
+      })
+      .catch((err) => console.error("Error loading local news JSON:", err));
+  }, []);
 
+  return (
+    <>
+      <Header />
+      <Breadcrumb
+        title="News and Updates"
+        image="https://lh3.googleusercontent.com/pw/AP1GczPUCocKMxC4wGVcuARVjp01biMREjUpBLF8kb06utd3X07oBJV-Avdh3tiqrcCtff9ZoFv2FDL3Cntewt5Kq6NDxapUD8XKAf8RIR-9s2EwdWKfLaw"
+      />
+      <br />
+      <br />
 
-<section id="recent-posts" className="recent-posts sections-bg">
-      <div className="container" data-aos="fade-up">
+      <section id="recent-posts" className="recent-posts sections-bg">
+        <div className="container" data-aos="fade-up">
+          <div className="section-header">
+            <h2>News and Updates</h2>
+            <p>
+              Find out the most exciting and interesting information from our
+              blog.
+            </p>
+          </div>
 
-        <div className="section-header">
-          <h2>News and Updates</h2>
-          <p>Find out the most exciting and interesting information from our blog.</p>
+          <center>
+            {loading ? (
+              <a></a>
+            ) : (
+              <a>
+                <div>
+                  <Skeleton width={300} height={30} />
+                  <Skeleton width={300} />
+                  <Skeleton count={3} />
+                </div>
+              </a>
+            )}
+          </center>
+
+          <div className="row gy-4">
+            {blog.map((blogger, index) => (
+              <div className="col-xl-4 col-md-6" key={index}>
+                <article onClick={() => handleInfo(blogger)}>
+                  <div className="post-img">
+                    <img
+                      src={blogger.news_img}
+                      alt=""
+                      className="img-fluid"
+                    />
+                  </div>
+
+                  <p className="post-category">{blogger.news_category}</p>
+
+                  <h2 className="title">
+                    <a href="#/">{blogger.news_heading}</a>
+                  </h2>
+                  <p>Find out ...</p>
+
+                  <div className="d-flex align-items-center">
+                    <img
+                      src={require(`../img/${process.env.REACT_APP_LOGO}`)}
+                      alt=""
+                      className="img-fluid post-author-img flex-shrink-0"
+                    />
+                    <div className="post-meta">
+                      <a>Read More</a>
+                      <br />
+                      <a>{blogger.date}</a>
+                    </div>
+                  </div>
+                </article>
+              </div>
+            ))}
+          </div>
         </div>
-        <center>{loading ? <a></a> : <a>
-       <div>
-       <Skeleton width={300} height={30}/>
-       <Skeleton width={300}/>
-       <Skeleton count={3}/>
-        </div> 
-        </a>}</center>
+      </section>
 
-        <div className="row gy-4">
-        {blog.map(blogger=>(
-
- <div className="col-xl-4 col-md-6" key={blogger.id}>
- <article onClick={()=>{handleInfo(blogger)}}>
-
-   <div className="post-img">
-     <img src={blogger.news_img} alt="" className="img-fluid"/>
-   </div>
-
-   <p className="post-category">{blogger.news_category}</p>
-
-   <h2 className="title">
-     <a href="#/">{blogger.news_heading}</a>
-   </h2>
-   <p>Find out ...</p>
-   <div className="d-flex align-items-center">
-     <img src={require(`../img/${process.env.REACT_APP_LOGO}`)} alt="" className="img-fluid post-author-img flex-shrink-0"/>
-     <div className="post-meta">
-     <a>Read More</a>
-     <br/>
-     <a>{blogger.date}</a>
-     </div>
-   </div>
-
- </article>
-</div>
-))}
-         
-
-         
-        </div>
+      <div id="newsinfo" style={{ display: "none" }}>
+        <NewsInfo />
       </div>
-    </section>
- 
 
-<div id="newsinfo" style={{display:"none"}}>
-<NewsInfo />
-</div>
-<ScrollToTop smooth/>
-  <Footer />
-     </>
-    )
+      <ScrollToTop smooth />
+      <Footer />
+    </>
+  );
 }
