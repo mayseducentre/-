5cee8470f3a13326207ab46bce2a0842b812f6fb
Asvan-Launcher <95467525+Amzi-kwasi@users.noreply.github.com>
2025-11-02 09:21:00 +0000

    Add line breaks in news_info componenty

--- src/component/news_info.js
+++ src/component/news_info.js
@@ -4,7 +4,8 @@ export default function NewsInfo(){
         <div style={{paddingLeft:"30px",paddingRight:"30px"}}>
 
         <button onClick={()=>{window.location.reload()}} style={{borderRadius:"20px",padding:"10px 25px",border:"1px solid orange",background:"transparent",color:"black"}}><i className="fa fa-arrow-left"></i> Back</button>
-            
+            <br/>
+    <br/>
         <h2 id="newsheader"></h2>
         <a id="newsdate"></a>|<a id="newscategory"></a>
         <br/>
