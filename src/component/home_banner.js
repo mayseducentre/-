018e9537037ecaaac02c2e321c415db188c10dc1
Asvan-Launcher <95467525+Amzi-kwasi@users.noreply.github.com>
2025-12-11 19:13:51 +0000

    Update home_banner.js

--- src/component/home_banner.js
+++ src/component/home_banner.js
@@ -5,7 +5,7 @@ export default function HomeBanner() {
   const banner = {
     media: {
       type: "image", // "image" or "video"
-      src: "https://www.w3schools.com/html/mov_bbb.mp4",
+      src: "https://lh3.googleusercontent.com/pw/AP1GczMCbPwvfxUD6yYl5JSD6Q9qpXN8dLh_8v0M1oy2Ezij0M-dIEa2YIR8HYZfn7JKZhL36NhMbwa5-uQSFTDaBk4HKwiiVa9vmSrvkdZoWo8nOWfgzDo",
       poster:
         "https://lh3.googleusercontent.com/pw/AP1GczMCbPwvfxUD6yYl5JSD6Q9qpXN8dLh_8v0M1oy2Ezij0M-dIEa2YIR8HYZfn7JKZhL36NhMbwa5-uQSFTDaBk4HKwiiVa9vmSrvkdZoWo8nOWfgzDo",
     },
