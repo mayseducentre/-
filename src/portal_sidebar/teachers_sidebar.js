f429c219433d1f3a7b51c792a3c8fd7104b7ed3a
Asvan-Launcher <95467525+Amzi-kwasi@users.noreply.github.com>
2025-12-26 16:36:54 +0000

    Update teachers_sidebar.js

--- src/portal_sidebar/teachers_sidebar.js
+++ src/portal_sidebar/teachers_sidebar.js
@@ -15,7 +15,7 @@ export default function Teachersidebar({ user, setActiveTab, activeTab }) {
     { label: "Dashboard", tab: "dashboard", icon: "🏠" },
     { label: "Assignments", tab: "assignments", icon: "📝" },
     { label: "Lessons", tab: "lessons", icon: "📘" },
-    { label: "Gradebook", tab: "gradebook", icon: "📊" },
+    { label: "Mecai", tab: "mecai", icon: 🕹" },
     { label: "Assessment", tab: "assessment", icon: "🧠" },
     { label: "Register", tab: "register", icon: "📋" },
     { label: "Announcements", tab: "announcements", icon: "📢" },
