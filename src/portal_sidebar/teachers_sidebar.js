adbaa0f9193ccb9ab2a45048790477ae6fd0c999
Asvan-Launcher <95467525+Amzi-kwasi@users.noreply.github.com>
2025-12-27 18:37:57 +0000

    Update teachers_sidebar.js

--- src/portal_sidebar/teachers_sidebar.js
+++ src/portal_sidebar/teachers_sidebar.js
@@ -28,7 +28,7 @@ export default function Teachersidebar({ user, setActiveTab, activeTab }) {
     try {
       await signOut(auth);
       // Optionally redirect after sign out
-      window.location.href = "#/portal"; 
+      window.location.reload()
     } catch (err) {
       console.error("Sign out failed:", err);
       alert("Failed to sign out. Please try again.");
