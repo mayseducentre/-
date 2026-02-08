8b23860aca5bdee03b03ecf450864246e2f6ff26
Asvan-Launcher <95467525+Amzi-kwasi@users.noreply.github.com>
2026-02-08 11:40:53 +0000

    Update student_sidebar.js

--- src/portal_sidebar/student_sidebar.js
+++ src/portal_sidebar/student_sidebar.js
@@ -116,7 +116,7 @@ const styles = {
     top: 0,
     display: "flex",
     flexDirection: "column",
-    z-index:"999",
+    z-index:999
   },
   profile: {
     textAlign: "center",
@@ -164,7 +164,7 @@ const styles = {
     height: 85,
     background: "#ffffff",
     borderTop: "1px solid #ddd",
-    z-index:"999",
+    z-index:999
   },
   scrollRow: {
     display: "flex",
